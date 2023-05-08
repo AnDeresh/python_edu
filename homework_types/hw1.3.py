33e738a07e2bd481dbf7f379c751394c819d1d34
anderesh <dereshivska@gmail.com>
2023-05-08 17:08:41 +0300

    is balanced upd

--- homework_types/hw1.3.py
+++ homework_types/hw1.3.py
@@ -1,14 +1,17 @@
-def is_balanced(string: str) -> bool:
-    res = 0
-    for elem in string:
-        if elem == '(':
-            res += 1
-        elif elem == ')':
-            res -= 1
-        if res < 0:
+def is_balanced(str) -> bool:
+    res_1 = 0
+    res_2 = 0
+    res_3 = 0
+    data_dict_1 = {'(': 1, ')': -1}
+    data_dict_2 = {'[': 1, ']': -1}
+    data_dict_3 = {'{': 1, '}': -1}
+    for i in str:
+        res_1 += data_dict_1.get(i, 0)
+        res_2 += data_dict_2.get(i, 0)
+        res_3 += data_dict_3.get(i, 0)
+        if res_1 < 0 or res_2 < 0 or res_3 < 0:
             return False
-    return res == 0
-
+    return res_1 == 0 and res_2 == 0 and res_3 == 0
 
 if __name__ == '__main__':
     assert is_balanced("((((((2+2))))))") is True
@@ -19,7 +22,6 @@ if __name__ == '__main__':
     assert is_balanced(")()()()(") is False
     assert is_balanced("(2+2") is False
     assert is_balanced("()((2+2)(())()") is False
-'''
     assert is_balanced("[((((((2+2))))))]") is True
     assert is_balanced("[(2+2)]/(2**2)") is True
     assert is_balanced("[{(2/4 + (5**2))}]") is True
@@ -27,5 +29,5 @@ if __name__ == '__main__':
     assert is_balanced("(((1*2)])") is False
     assert is_balanced(")()()()(") is False
     assert is_balanced("](2+2") is False
+    assert is_balanced("(2+2]") is False
     assert is_balanced("{()(2+2)(())()]") is False
-'''
